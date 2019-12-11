05bf70d2821a8057737e98ecd712f03270d13e87
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2019-12-11 16:31:32 +0100

    [ADD] Include navigation views on app

--- app/navigations/Navigation.tsx
+++ app/navigations/Navigation.tsx
@@ -4,22 +4,77 @@ import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
 import RestaurantsScreenStacks from "./RestaurantsStacks";
+import TopListsScreenStacks from "./TopListsStacks";
+import SearchScreenStacks from "./SearchStacks";
+import MyAccountScreenStacks from "./MyAccountStacks";
 
-const NavigationStacks = createBottomTabNavigator({
-    Restaurants: {
-        screen: RestaurantsScreenStacks,
-        navigationOptions: () => ({
-            tabBarLabel: "Restaurants",
-            tabBarIcon: ({ tintColor }) => (
-                <Icon
-                    type="material-community"
-                    name="compass-outline"
-                    size={22}
-                    color={tintColor}
-                />
-            )
-        })
+const NavigationStacks = createBottomTabNavigator(
+    {
+        Restaurants: {
+            screen: RestaurantsScreenStacks,
+            navigationOptions: () => ({
+                tabBarLabel: "Restaurants",
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon
+                        type="material-community"
+                        name="compass-outline"
+                        size={22}
+                        color={tintColor}
+                    />
+                )
+            })
+        },
+        TopLists: {
+            screen: TopListsScreenStacks,
+            navigationOptions: () => ({
+                tabBarLabel: "Ranking",
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon
+                        type="material-community"
+                        name="star-outline"
+                        size={22}
+                        color={tintColor}
+                    />
+                )
+            })
+        },
+        Search: {
+            screen: SearchScreenStacks,
+            navigationOptions: () => ({
+                tabBarLabel: "Search",
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon
+                        type="material-community"
+                        name="magnify"
+                        size={22}
+                        color={tintColor}
+                    />
+                )
+            })
+        },
+        MyAccount: {
+            screen: MyAccountScreenStacks,
+            navigationOptions: () => ({
+                tabBarLabel: "My account",
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon
+                        type="material-community"
+                        name="account"
+                        size={22}
+                        color={tintColor}
+                    />
+                )
+            })
+        }
+    },
+    {
+        initialRouteName: "Restaurants",
+        order: ["Restaurants", "TopLists", "Search", "MyAccount"],
+        tabBarOptions: {
+            inactiveTintColor: "#646464",
+            activeTintColor: "#00A680"
+        }
     }
-});
+);
 
 export default createAppContainer(NavigationStacks);
