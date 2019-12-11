ec73b275794f1a566fcc043cea14746080cec5c1
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2019-12-11 13:02:54 +0100

    [ADD] React native elements library

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>5 Tenedores</Text>
+      <Button 
+        title="Click me" 
+        loading={true}
+      />
     </View>
   );
 }
