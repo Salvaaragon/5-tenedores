227777d52e9dcea3c323e7b61a02acb9e715f626
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2019-12-13 13:24:54 +0100

    [ADD] Logout button

--- app/screens/Account/UserLogged.tsx
+++ app/screens/Account/UserLogged.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import * as firebase from "firebase";
 
 export default function UserLogged() {
     return (
         <View>
-            <Text>User Logged...</Text>
+            <Text>User logged...</Text>
+            <Button title="Logout" onPress={() => firebase.auth().signOut()} />
         </View>
     );
 }
