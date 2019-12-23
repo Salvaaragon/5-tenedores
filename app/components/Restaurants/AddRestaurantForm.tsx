cd406c832970585aaaa3968a158afede802e646c
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2019-12-23 14:11:32 +0100

    [ADD] Create restaurant form

---
+++ app/components/Restaurants/AddRestaurantForm.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ScrollView, Alert, Dimensions } from "react-native";
+import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+
+export default function AddRestaurantForm(props) {
+    const { toastRef, setIsLoading, navigation } = props;
+    const [imagesSelected, setImagesSelected] = useState([]);
+
+    return (
+        <ScrollView>
+            <UploadImage
+                imagesSelected={imagesSelected}
+                setImagesSelected={setImagesSelected}
+                toastRef={toastRef}
+            />
+        </ScrollView>
+    );
+}
+
+function UploadImage(props) {
+    const { imagesSelected, setImagesSelected, toastRef } = props;
+
+    const imageSelect = async () => {
+        const resultPermission = await Permissions.askAsync(
+            Permissions.CAMERA_ROLL
+        );
+        const resultPermissionCamera =
+            resultPermission.permissions.cameraRoll.status;
+
+        if (resultPermissionCamera === "denied") {
+            toastRef.current.show(
+                "You need to accept gallery permissions",
+                3000
+            );
+        } else {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [4, 3]
+            });
+
+            if (result.cancelled) {
+                toastRef.current.show(
+                    "You have closed image gallery without select an image",
+                    2000
+                );
+            } else {
+                setImagesSelected([...imagesSelected, result.uri]);
+            }
+        }
+    };
+
+    return (
+        <View style={styles.viewImages}>
+            <Icon
+                type="material-community"
+                name="camera"
+                color="#7A7A7A"
+                containerStyle={styles.containerIcon}
+                onPress={imageSelect}
+            />
+
+            <Avatar
+                onPress={() => console.log("Deleting image")}
+                style={styles.miniatureStyle}
+                // source={{}}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewImages: {
+        flexDirection: "row",
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 30
+    },
+    containerIcon: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginRight: 10,
+        height: 70,
+        width: 70,
+        backgroundColor: "#E3E3E3"
+    },
+    miniatureStyle: {
+        width: 70,
+        height: 70,
+        marginRight: 10
+    }
+});
