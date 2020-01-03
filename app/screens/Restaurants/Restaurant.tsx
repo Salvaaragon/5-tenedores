31cf08e3127cb14ab0bf4d6ca1a1a682f53fbd7b
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-03 12:45:38 +0100

    [ADD] Get restaurant images from firebase

--- app/screens/Restaurants/Restaurant.tsx
+++ app/screens/Restaurants/Restaurant.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import * as firebase from "firebase";
 
 export default function Restaurant(props) {
     const { navigation } = props;
     const { restaurant } = navigation.state.params.restaurant.item;
+    const [imagesRestaurant, setImagesRestaurant] = useState([]);
+
+    useEffect(() => {
+        const arrayUrls = [];
+
+        (async () => {
+            await Promise.all(
+                restaurant.images.map(async idImage => {
+                    await firebase
+                        .storage()
+                        .ref(`restaurant-images/${idImage}`)
+                        .getDownloadURL()
+                        .then(imageUrl => {
+                            arrayUrls.push(imageUrl);
+                        });
+                })
+            );
+            setImagesRestaurant(arrayUrls);
+        })();
+    }, []);
 
     return (
         <View>
