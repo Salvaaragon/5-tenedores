293063e1c303dada52229d3cc64bb194218e62cb
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-07 09:17:43 +0100

    [ADD] Rating restaurant component on add review form

--- app/screens/Restaurants/AddReviewRestaurant.tsx
+++ app/screens/Restaurants/AddReviewRestaurant.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { AirbnbRating, Button } from "react-native-elements";
 
 export default function AddReviewRestaurant(props) {
     const { navigation } = props;
     const idRestaurant = navigation.state.params.idRestaurant;
+    const [rating, setRating] = useState(null);
+
+    const addReview = () => {
+        console.log(rating);
+    };
 
     return (
-        <View>
-            <Text>AddReviewRestaurant...</Text>
+        <View style={styles.viewBody}>
+            <View style={styles.viewRating}>
+                <AirbnbRating
+                    count={5}
+                    defaultRating={0}
+                    size={30}
+                    onFinishRating={value => setRating(value)}
+                />
+            </View>
+            <View>
+                <Button title="Send comment" onPress={addReview} />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1
+    },
+    viewRating: {
+        height: 110,
+        backgroundColor: "#F2F2F2"
+    }
+});
