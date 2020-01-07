b38c65134464f24e5bcffa8014b6e251152d93d9
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-07 13:31:21 +0100

    [ADD] Add restaurant to favourites list logic

--- app/screens/Restaurants/Restaurant.tsx
+++ app/screens/Restaurants/Restaurant.tsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, View, ScrollView, Text, Dimensions } from "react-native";
 import { Rating, Icon, ListItem } from "react-native-elements";
 import CustomCarousel from "../../components/CustomCarousel";
 import Map from "../../components/Map";
 import ListReviews from "../../components/Restaurants/ListReviews";
-import * as firebase from "firebase";
+import Toast from "react-native-easy-toast";
+
+import { firebaseApp } from "../../utils/FireBase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+const db = firebase.firestore(firebaseApp);
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -14,6 +19,7 @@ export default function Restaurant(props) {
     const [imagesRestaurant, setImagesRestaurant] = useState([]);
     const [rating, setRating] = useState(restaurant.rating);
     const [isFavourite, setIsFavourite] = useState(false);
+    const toastRef = useRef();
 
     useEffect(() => {
         const arrayUrls = [];
@@ -35,7 +41,26 @@ export default function Restaurant(props) {
     }, []);
 
     const addFavourite = () => {
-        setIsFavourite(true);
+        const payload = {
+            idUser: firebase.auth().currentUser.uid,
+            idRestaurant: restaurant.id
+        };
+
+        db.collection("favourites")
+            .add(payload)
+            .then(() => {
+                setIsFavourite(true);
+                toastRef.current.show(
+                    "Restaurant added to your favourites list",
+                    2000
+                );
+            })
+            .catch(() => {
+                toastRef.current.show(
+                    "Error adding restaurant to your favourites list. Try again later",
+                    2000
+                );
+            });
     };
 
     const removeFavourite = () => {
@@ -74,6 +99,7 @@ export default function Restaurant(props) {
                 idRestaurant={restaurant.id}
                 setRating={setRating}
             />
+            <Toast ref={toastRef} position="center" opacity={0.5} />
         </ScrollView>
     );
 }
