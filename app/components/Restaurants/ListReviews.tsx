a23ea17e7908981fdb731480e8ad0f81b21be99a
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-07 11:38:59 +0100

    [ADD] Get correct rating on load restaurant info screen

--- app/components/Restaurants/ListReviews.tsx
+++ app/components/Restaurants/ListReviews.tsx
@@ -2,8 +2,45 @@ import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
 import { Button, Avatar, Rating } from "react-native-elements";
 
+import { firebaseApp } from "../../utils/FireBase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+const db = firebase.firestore(firebaseApp);
+
 export default function ListReviews(props) {
     const { navigation, idRestaurant } = props;
+    const [reviews, setReviews] = useState([]);
+    const [reviewsReload, setReviewsReload] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const resultReviews = [];
+            const arrayRating = [];
+
+            db.collection("reviews")
+                .where("idRestaurant", "==", idRestaurant)
+                .get()
+                .then(response => {
+                    response.forEach(doc => {
+                        resultReviews.push(doc.data());
+                        arrayRating.push(doc.data().rating);
+                    });
+
+                    let numSum = 0;
+                    arrayRating.map(value => {
+                        numSum = numSum + value;
+                    });
+
+                    const countRating = arrayRating.length;
+                    const resultRating = numSum / countRating;
+                    const resultRatingFinish = resultRating ? resultRating : 0;
+
+                    setReviews(resultReviews);
+                });
+
+            setReviewsReload(false);
+        })();
+    }, [reviewsReload]);
 
     return (
         <View>
