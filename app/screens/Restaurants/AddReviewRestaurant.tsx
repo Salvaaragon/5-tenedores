424a95c2c6d788019cca3b86a11c8d9963d1ae48
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-07 10:52:20 +0100

    [ADD] Restaurant review form structure

--- app/screens/Restaurants/AddReviewRestaurant.tsx
+++ app/screens/Restaurants/AddReviewRestaurant.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { AirbnbRating, Button } from "react-native-elements";
+import { StyleSheet, View } from "react-native";
+import { AirbnbRating, Button, Input } from "react-native-elements";
 
 export default function AddReviewRestaurant(props) {
     const { navigation } = props;
     const idRestaurant = navigation.state.params.idRestaurant;
     const [rating, setRating] = useState(null);
+    const [title, setTitle] = useState("");
+    const [review, setReview] = useState("");
 
     const addReview = () => {
         console.log(rating);
@@ -21,8 +23,24 @@ export default function AddReviewRestaurant(props) {
                     onFinishRating={value => setRating(value)}
                 />
             </View>
-            <View>
-                <Button title="Send comment" onPress={addReview} />
+            <View style={styles.formReview}>
+                <Input
+                    placeholder="Title"
+                    containerStyle={styles.input}
+                    onChange={e => setTitle(e.nativeEvent.text)}
+                />
+                <Input
+                    placeholder="Comment"
+                    multiline={true}
+                    inputContainerStyle={styles.textarea}
+                    onChange={e => setReview(e.nativeEvent.text)}
+                />
+                <Button
+                    title="Send comment"
+                    onPress={addReview}
+                    containerStyle={styles.btnContainer}
+                    buttonStyle={styles.btn}
+                />
             </View>
         </View>
     );
@@ -35,5 +53,30 @@ const styles = StyleSheet.create({
     viewRating: {
         height: 110,
         backgroundColor: "#F2F2F2"
+    },
+    formReview: {
+        margin: 10,
+        marginTop: 40,
+        flex: 1,
+        alignItems: "center"
+    },
+    input: {
+        marginBottom: 10
+    },
+    textarea: {
+        height: 150,
+        width: "100%",
+        padding: 0,
+        margin: 0
+    },
+    btnContainer: {
+        flex: 1,
+        justifyContent: "center",
+        marginTop: 20,
+        marginBottom: 10,
+        width: "95%"
+    },
+    btn: {
+        backgroundColor: "#00A680"
     }
 });
