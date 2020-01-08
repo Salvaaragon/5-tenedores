a7f56fc363d613a8a7a00a9554dc42d4a0bb7a86
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-08 11:36:48 +0100

    [ADD] Search bar

--- app/screens/Search.tsx
+++ app/screens/Search.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import { SearchBar, ListItem, Icon } from "react-native-elements";
+import * as firebase from "firebase";
 
-export default function Search() {
+export default function Search(props) {
+    const { navigation } = props;
+    const [restaurants, setRestaurants] = useState([]);
+    const [search, setSearch] = useState("");
+
     return (
         <View>
-            <Text>We are on searcher</Text>
+            <SearchBar
+                placeholder="Search restaurant..."
+                onChangeText={e => setSearch(e)}
+                value={search}
+                containerStyle={styles.searchBar}
+            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    searchBar: {
+        marginBottom: 20
+    }
+});
