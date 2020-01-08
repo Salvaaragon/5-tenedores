be84f0e4b2007eee1dccfac1159c99b6d506e5d0
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-08 11:45:44 +0100

    [ADD] Get restaurant from firebase database

--- app/screens/Search.tsx
+++ app/screens/Search.tsx
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import { SearchBar, ListItem, Icon } from "react-native-elements";
 import * as firebase from "firebase";
+import { FireSQL } from "firesql";
+import firebaseApp from "firebase/app";
+
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: "id" });
 
 export default function Search(props) {
     const { navigation } = props;
     const [restaurants, setRestaurants] = useState([]);
     const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        if (search) {
+            fireSQL
+                .query(`SELECT * FROM restaurants WHERE name LIKE '${search}%'`)
+                .then(response => {
+                    setRestaurants(response);
+                });
+        }
+    }, [search]);
 
     return (
         <View>
