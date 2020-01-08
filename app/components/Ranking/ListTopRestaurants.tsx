76983a2544fc458b55e0c1059141e1db490e46e3
Salvador Aragón Reyes <salvador.aragon@opinno.com>
2020-01-08 10:50:30 +0100

    [ADD] Get each restaurant image from database

--- app/components/Ranking/ListTopRestaurants.tsx
+++ app/components/Ranking/ListTopRestaurants.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     StyleSheet,
     View,
@@ -10,11 +10,40 @@ import { Card, Image, Icon, Rating } from "react-native-elements";
 import * as firebase from "firebase";
 
 export default function ListTopRestaurants(props) {
-    const { restaurants, navigation } = props;
+    const { navigation, restaurants } = props;
+
+    return (
+        <FlatList
+            data={restaurants}
+            renderItem={restaurant => (
+                <Restaurant restaurant={restaurant} navigation={navigation} />
+            )}
+            keyExtractor={(item, idx) => idx.toString()}
+        />
+    );
+}
+
+function Restaurant(props) {
+    const { restaurant, navigation } = props;
+    const { name, description, images, rating } = restaurant.item;
+    const [imageRestaurant, setImageRestaurant] = useState(null);
+
+    useEffect(() => {
+        const image = images[0];
+        firebase
+            .storage()
+            .ref(`restaurant-images/${image}`)
+            .getDownloadURL()
+            .then(response => {
+                setImageRestaurant(response);
+            });
+    }, []);
 
     return (
         <View>
-            <Text>ListTopRestaurants</Text>
+            <Text>Restaurant</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({});
